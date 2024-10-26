8bd399c75b54b82644d14039cc9772e897b13e4f
Chessfreewebsiteforplaying <kenny240613@gmail.com>
2024-10-26 22:19:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+// script.js
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+canvas.width = 800;
+canvas.height = 600;
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // Vẽ nhân vật, đối thủ, v.v.
+}
+
+function update() {
+    // Cập nhật trạng thái game
+    draw();
+    requestAnimationFrame(update);
+}
+
+update();
